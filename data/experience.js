b301bfc03420a3fc0aa2a8553d5a08a0f553a38a
azze-ddine <azzeddineitsaf@gmail.com>
2026-02-02 11:53:40 +0100

    updat

--- data/experience.js
+++ data/experience.js
@@ -1,25 +1,31 @@
+import { positionalKeys } from "framer-motion";
+
 export const experience = [
   {
     role: "IT-Specialist Intern",
     company: "TOTAL-IT-GLOBAL",
+    location: "Salé, Morocco",
     period: "14 NOV 2024 - 14 Apr 2025",
     description: "Migration of IBM Guardium to Imperva DAM with security policy configuration, reducing security risks and optimizing network monitoring.",
   },
   {
     role: "Full-Stack Developer Intern",
     company: "G-SOFT",
+    location: "Rabat, Morocco",
     period: "18 Apr 2024 - 08 Jun 2024",
     description: "Designed and developed a Help Center web application to enhance user communication.",
   },
   {
     role: "Web Developer Intern",
     company: "ADN",
+    location: "Rabat, Morocco",
     period: "01 Jun 2020 - 01 Aug 2024",
     description: "Developed and maintained a web application for managing medical records, human resources, and administrative tasks.",
   },
   {
     role: "Freelance Full-Stack Developer",
     company: "Self-Employed",
+    location: "Remote",
     period: "01 Sep 2024 - Present",
     description: "Developed and maintained multiple web applications for clients across various industries.",
   }
--- data/news.js
+++ data/news.js
@@ -1,15 +1,15 @@
 export const news = [
   {
-    date: "3 juil. 2025",
-    title: "Cybersecurity Internship at DATAPROTECT: Database Activity Monitoring Implementation",
-    excerpt: "Completed a 4-month intensive cybersecurity internship at DATAPROTECT, specializing in Database Activity Monitoring solutions...",
-    link: "#"
+    date: "12 sep. 2024",
+    title: "SupHackthon Internship at SupMTI: creating a platform to showcase innovative and inspiring projects",
+    excerpt: "48 intense hours, teams made up of future engineers and managers pooled their skills and determination to address real problems through innovative solutions...",
+    link: "https://supmti.ac.ma/succes-retentissant-pour-le-suphackathon-de-lecole-supmti-rabat/"
   },
   {
-    date: "16 avr. 2025",
-    title: "GITEX Africa 2025: Immersion in Global Technology Innovation",
-    excerpt: "Participated in GITEX Africa 2025, the continent's largest tech exhibition, engaging with cutting-edge AI solutions, future tech...",
-    link: "#"
+    date: "25 nov. 2025",
+    title: "CloudTech'25: 7th International Conference on Cloud Computing and Artificial Intelligence",
+    excerpt: "Participated in CloudTech'25, the continent's largest tech exhibition, engaging with Cloud Computing AI solutions, future tech...",
+    link: "https://ensias.um5.ac.ma/news/7th-international-conference-cloud-computing-and-artificial-intelligence-technologies-and-0"
   },
   {
     date: "4 juin 2024",
